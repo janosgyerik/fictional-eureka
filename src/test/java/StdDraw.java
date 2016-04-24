2ec30516c427569cf5452b47e445f2834c157035
Janos Gyerik <janos@kronos>
2016-04-24 08:26:33 +0200

    added provided Point and created dummy impl of dependencies

---
+++ src/test/java/StdDraw.java
@@ -0,0 +1,25 @@
+public class StdDraw {
+    private static double penRadius;
+
+    public static void point(double x, double y) {
+    }
+
+    static void line(double x, double y, double x1, double y1) {
+    }
+
+    public static void setCanvasSize(int w, int h) {
+
+    }
+
+    public static void setXscale(int i, int w) {
+
+    }
+
+    public static void setYscale(int i, int h) {
+
+    }
+
+    public static void setPenRadius(double penRadius) {
+        StdDraw.penRadius = penRadius;
+    }
+}
---
+++ src/test/java/StdIn.java
@@ -0,0 +1,13 @@
+public class StdIn {
+    public static int readInt() {
+        return 0;
+    }
+
+    public static boolean isEmpty() {
+        return false;
+    }
+
+    public static double readDouble() {
+        return 0;
+    }
+}
