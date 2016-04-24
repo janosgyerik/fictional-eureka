acbcf84a11533c7f9955c59bc573a5e8eaf42bad
Janos Gyerik <janos@kronos>
2016-04-24 10:58:36 +0200

    added comments

--- src/test/java/Tour.java
+++ src/test/java/Tour.java
@@ -28,6 +28,7 @@ public class Tour {
         add(a, b, c, d);
     }
 
+    // helper method to simply append points
     private void add(Point... points) {
         Node node = start;
         while (node.next != null) {
@@ -61,22 +62,29 @@ public class Tour {
 
     // return the total distance of the tour
     public double distance() {
-        double distance = 0;
+        double total = 0;
 
+        // set to the first point
         Point prev = start.next.p;
+
+        // set to the second node
         Node node = start.next.next;
         while (node != null) {
-            distance += prev.distanceTo(node.p);
+            // add to total the distance from prev to current
+            total += prev.distanceTo(node.p);
+
+            // update prev to current, for the next cycle
             prev = node.p;
             node = node.next;
         }
-        distance += prev.distanceTo(start.next.p);
-        return distance;
+        // add to total the distance from last to first
+        total += prev.distanceTo(start.next.p);
+
+        return total;
     }
 
     /*
-    Insert p using nearest neighbor heuristic
-
+    Nearest neighbor heuristic:
     Read in the next point, and add it to the current tour after the point to which it is closest.
     (If there is more than one point to which it is closest, insert it after the first such point you discover.)
      */
@@ -85,15 +93,18 @@ public class Tour {
         insertAfter(nearest, p);
     }
 
+    // helper method to find the Node nearest to p
     private Node findNearest(Point p) {
-        double min = Double.MAX_VALUE;
+        // set minimum distance to "infinity"
+        double minDistance = Double.MAX_VALUE;
 
         Node nearest = start;
         Node node = start;
         while (node.next != null) {
             double distance = p.distanceTo(node.next.p);
-            if (distance < min) {
-                min = distance;
+            if (distance < minDistance) {
+                // this node is the nearest so far
+                minDistance = distance;
                 nearest = node.next;
             }
             node = node.next;
@@ -101,14 +112,16 @@ public class Tour {
         return nearest;
     }
 
-    private void insertAfter(Node current, Point point) {
-        current.next = new Node(point, current.next);
+    // helper method to insert a Point after a Node, and udate the Tour size
+    private void insertAfter(Node node, Point point) {
+        node.next = new Node(point, node.next);
         ++size;
     }
 
     /*
-    Smallest increase heuristic:  Read in the next point, and add it to the current tour
-    after the point where it results in the least possible increase in the tour length.
+    Smallest increase heuristic:
+    Read in the next point, and add it to the current tour after the point where
+    it results in the least possible increase in the tour length.
     (If there is more than one point, insert it after the first such point you discover.)
      */
     public void insertSmallest(Point p) {
@@ -116,34 +129,42 @@ public class Tour {
         insertAfter(smallestIncrease, p);
     }
 
+    // helper method to find the Node after which the increase will be smallest
     private Node findSmallestIncrease(Point p) {
         if (start.next == null) {
+            // the tour is empty, so should insert after the start marker
             return start;
         }
 
-        double min = Double.MAX_VALUE;
+        // set minimum increase to "infinity"
+        double minIncrease = Double.MAX_VALUE;
 
         Node smallestIncrease = start;
+        // set to first node
         Node node = start.next;
+
         while (node.next != null) {
-            double existing = node.p.distanceTo(node.next.p);
+            double distCurrentToNext = node.p.distanceTo(node.next.p);
             double distCurrentToNew = node.p.distanceTo(p);
             double distNextToNew = node.next.p.distanceTo(p);
 
-            double increase = -existing + distCurrentToNew + distNextToNew;
-            if (increase < min) {
-                min = increase;
+            // the increase = replace the link from A->B, with A->X->B
+            double increase = -distCurrentToNext + distCurrentToNew + distNextToNew;
+
+            if (increase < minIncrease) {
+                minIncrease = increase;
                 smallestIncrease = node;
             }
             node = node.next;
         }
 
-        double existing = node.p.distanceTo(start.next.p);
+        // special treatment for last Node: replace LAST->FIRST with LAST->X->FIRST
+        double distCurrentToNext = node.p.distanceTo(start.next.p);
         double distCurrentToNew = node.p.distanceTo(p);
         double distNextToNew = start.next.p.distanceTo(p);
 
-        double increase = -existing + distCurrentToNew + distNextToNew;
-        if (increase < min) {
+        double increase = -distCurrentToNext + distCurrentToNew + distNextToNew;
+        if (increase < minIncrease) {
             smallestIncrease = node;
         }
 
