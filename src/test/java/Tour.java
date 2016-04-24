b04847ea5182e1a850fb98f44753b8ee0c75733c
Janos Gyerik <janos@kronos>
2016-04-24 10:14:43 +0200

    NearestInsertion done

--- src/test/java/Tour.java
+++ src/test/java/Tour.java
@@ -8,6 +8,11 @@ public class Tour {
         Node(Point p) {
             this.p = p;
         }
+
+        Node(Point p, Node next) {
+            this(p);
+            this.next = next;
+        }
     }
 
     private final Node start = new Node();
@@ -57,12 +62,49 @@ public class Tour {
 
     // return the total distance of the tour
     public double distance() {
-        return 0;
+        double distance = 0;
+
+        Point prev = start.next.p;
+        Node runner = start.next.next;
+        while (runner != null) {
+            distance += prev.distanceTo(runner.p);
+            prev = runner.p;
+            runner = runner.next;
+        }
+        distance += prev.distanceTo(start.next.p);
+        return distance;
     }
 
-    // insert p using nearest neighbor heuristic
-    public void insertNearest(Point p) {
+    /*
+    Insert p using nearest neighbor heuristic
 
+    Read in the next point, and add it to the current tour after the point to which it is closest.
+    (If there is more than one point to which it is closest, insert it after the first such point you discover.)
+     */
+    public void insertNearest(Point p) {
+        Node nearest = findNearest(p);
+        insertAfter(nearest, p);
+    }
+
+    private Node findNearest(Point p) {
+        double min = Double.MAX_VALUE;
+
+        Node nearest = start;
+        Node runner = start;
+        while (runner.next != null) {
+            double distance = p.distanceTo(runner.next.p);
+            if (distance < min) {
+                min = distance;
+                nearest = runner.next;
+            }
+            runner = runner.next;
+        }
+        return nearest;
+    }
+
+    private void insertAfter(Node current, Point point) {
+        current.next = new Node(point, current.next);
+        ++size;
     }
 
     // insert p using smallest increase heuristic
