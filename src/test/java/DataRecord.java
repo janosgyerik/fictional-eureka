086c717997b6b628a2c33d350a0e1b7945442899
Janos Gyerik <janos@kronos>
2016-04-24 14:51:58 +0200

    removed more unused stuff

--- src/test/java/DataRecord.java
+++ src/test/java/DataRecord.java
@@ -1,20 +1,7 @@
 public class DataRecord {
 
-    public static final int NEVER_USED = 0;
-    public static final int PREVIOUSLY_USED = 1;
-    public static final DataRecord maxLength = null;
     private DataRecord next;
     private String name;
-    private int used;
-
-
-    public int getUsed() {
-        return used;
-    }
-
-    public void setUsed(int used) {
-        this.used = used;
-    }
 
     public DataRecord getNext() {
         return next;
@@ -31,19 +18,5 @@ public class DataRecord {
     public void setName(String name) {
         this.name = name;
     }
-
-    public String toString() {
-        return "<" + name + "> -->";
-    }
-
-    public int getSize() {
-        int length = 0;
-        while (maxLength != null) {
-            length++;
-
-        }
-        return (length);
-    }
-
 
 }
