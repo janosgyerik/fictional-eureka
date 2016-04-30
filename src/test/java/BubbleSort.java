88a727fef8bb9c7b57004466f1beb8f8f6775dde
Janos Gyerik <janos@kronos>
2016-04-30 08:36:14 +0200

    bubble sort doesnt need to ignore case

--- src/test/java/BubbleSort.java
+++ src/test/java/BubbleSort.java
@@ -2,7 +2,7 @@ public class BubbleSort {
     public static void sort(String[] words) {
         for (int i = 0;  i < words.length - 1;  i++ ){
             for (int j = i + 1; j < words.length; j++) {
-                if (words[i].compareToIgnoreCase(words[j]) > 0) {
+                if (words[i].compareTo(words[j]) > 0) {
                     String temp = words[i];
                     words[i] = words[j];
                     words[j] = temp;
